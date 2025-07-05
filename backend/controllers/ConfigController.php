35bc3668b6508d3b798681de1b3dc61e70357ef5
eaglevisionsolutions <info@eaglevisionsolutions.ca>
2025-07-05 11:48:16 -0400

    fixed error where not returning jason to test

--- backend/controllers/ConfigController.php
+++ backend/controllers/ConfigController.php
@@ -1,9 +1,16 @@
+echo json_encode([
 <?php
 // backend/controllers/ConfigController.php
-require_once __DIR__ . '/../config.php';
-
-header('Content-Type: application/json');
-
-echo json_encode([
-    'APP_DEBUG' => (bool)getenv('APP_DEBUG') || (defined('APP_DEBUG') && APP_DEBUG === true)
-]);
+class ConfigController {
+    public function getAll() {
+        require_once __DIR__ . '/../config.php';
+        header('Content-Type: application/json');
+        $debug = (getenv('APP_DEBUG') === 'true') || (defined('APP_DEBUG') && APP_DEBUG === true);
+        echo json_encode([
+            'APP_DEBUG' => $debug
+        ]);
+    }
+    public function create($data) { http_response_code(405); }
+    public function update($data) { http_response_code(405); }
+    public function delete($id) { http_response_code(405); }
+}
